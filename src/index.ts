7b1a0da3360f821b78c3269336ce120c752c5e96
umarovali <umarovali.345@gmail.com>
2026-06-02 11:35:36 +0600

    ok

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://my-porftfolio-ali.netlify.app"],
   }),
 );
 
